76af80c12a7f2f8b028c89ae758156aadf015872
George Brassey <gbrassey@gmail.com>
2015-10-03 16:47:48 -0400

    --amend

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './index.scss';
 
 const SPOTIFY_CLIENT_ID = '1e0ad7166e5c4440848c13c4218eaeb3';
+const SPOTIFY_SCOPES = [
+  'playlist-read-private',
+  'user-library-read',
+  'playlist-read-collaborative',
+].join(' ');
 const SPOTIFY_LOGIN_URL = ``
-                            + `https://accounts.spotify.com/authorize?`
-                            + `client_id=${SPOTIFY_CLIENT_ID}`
-                            + `&response_type=token`
-                            + `&redirect_uri=${encodeURIComponent('http://localhost:8080/spotify-login-callback/')}`;
+  + `https://accounts.spotify.com/authorize?`
+  + `client_id=${SPOTIFY_CLIENT_ID}`
+  + `&response_type=token`
+  + `&redirect_uri=${encodeURIComponent('http://localhost:8080/spotify-login-callback/')}`
+  + `&scope=${SPOTIFY_SCOPES}`;
 
 export default class Login extends React.Component {
   render() {
