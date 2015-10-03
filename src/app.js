ccc77df136341e5e9a6d875a9e415b04bb1ef271
George Brassey <gbrassey@gmail.com>
2015-10-03 15:43:08 -0400

    imports bootstrap

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import Greeting from './components/greeting';
+import React from "react";
+import Greeting from "./components/greeting";
+
+global.jQuery = require("jquery");
+
+require("bootstrap-sass!../bootstrap-sass.config.js");
+import "./app.scss";
 
 React.render(
   <Greeting name="World" />,
-  document.getElementById('mount-node')
+  document.getElementById("mount-node")
 );
--- webpack.config.js
+++ webpack.config.js
@@ -13,21 +13,26 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         loaders: ["react-hot", "babel-loader"]
-      }, {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ["babel-loader"]
       }, {
         test: /\.html$/,
         loader: "file?name=[name].[ext]"
       }, {
-        test: /\.scss$/,
+        test: /bootstrap\/js\//,
+        loader: "imports?jQuery=jquery"
+      }, {
+        test: /\.(scss|css)$/,
         loaders: ["style", "css", "sass"]
       }, {
-        test: /\.(png|jpg|gif|woff)$/, loader: "url-loader?limit=100000"
+        test: /\.(png|jpg|gif)$/, loader: "url-loader?limit=100000"
+      },
+        { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+        { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"
       }
     ]
   }
