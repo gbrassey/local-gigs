0c74f878471a15a605ecbb8177445920848bf4b7
George Brassey <gbrassey@gmail.com>
2015-10-31 16:24:34 -0400

    adds sourcemaps :simple_smile:

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,37 @@
-var path = require("path");
+var path = require('path');
 
 module.exports = {
-  context: path.join(__dirname, "/src"),
+  context: path.join(__dirname, '/src'),
   entry: {
-    javascript: "./app.js",
-    html: "./index.html"
+    javascript: './app.js',
+    html: './index.html',
   },
   output: {
-    filename: "app.js",
-    path: path.join(__dirname, "/dist")
+    filename: 'app.js',
+    path: path.join(__dirname, '/dist'),
+    sourceMapFilename: '[name].js.map',
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ["react-hot", "babel-loader"]
+        loaders: ['react-hot', 'babel-loader'],
       }, {
         test: /\.html$/,
-        loader: "file?name=[name].[ext]"
+        loader: 'file?name=[name].[ext]',
       }, {
         test: /\.(scss|css)$/,
-        loaders: ["style", "css", "sass"]
+        loaders: ['style', 'css', 'sass'],
       }, {
-        test: /\.(png|jpg|gif)$/, loader: "url-loader?limit=100000"
+        test: /\.(png|jpg|gif)$/, loader: 'url-loader?limit=100000',
       },
-        { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-        { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"
-      }
-    ]
-  }
+        { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+        { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml',
+      },
+    ],
+  },
 };
